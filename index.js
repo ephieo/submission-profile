f1df022ec0b7a3a9a1d792f26e61f94e637d0c76
ephieo <60614102+ephieo@users.noreply.github.com>
2020-04-21 01:21:43 +0100

    creating a for loop to iterate
    -created the positionSlides function with slides and slideWidth as the arguments. 
    - created a for loop to iterate through the array containing the children of the carousel list/array.
    - slides[i] will move left  by the distance declared by "slideWidth * i +px ". 
    - the width of the image = slideWidth . 
    - this is because the image fills out the carousel container so the next image is slideWidth to the right and you need to move the image to the left by a certain distance.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 <script>
   const carousel = document.querySelector(".carousel");
 const slides = [...carousel.children];
+function positionSlides(slides,slideWidth){
+for (let i=0; i<slides.length;i++){
+slides[i].style.left=slideWidth * i +"px";
+}
+}
 </script>

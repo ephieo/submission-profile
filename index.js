81826ed30d991a0e289f767a0d73d2eaf979b4e2
ephieo <60614102+ephieo@users.noreply.github.com>
2020-04-21 01:45:31 +0100

    Update index.js
    removing script tags

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-<script>
-  const carousel = document.querySelector(".carousel");
+const carousel = document.querySelector(".carousel");
 const slides = [...carousel.children];
 function positionSlides(slides,slideWidth){
 for (let i=0; i<slides.length;i++){
@@ -9,4 +8,4 @@ slides[i].style.left=slideWidth * i +"px";
 const slideWidth = slides[0].getBoundClientRect().width;
 positionSlides(slides,slideWidth);
 const currentSlide = carousel.querySelector(".active");
-</script>
+

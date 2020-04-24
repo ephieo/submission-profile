9092b691cf61e534d284bed2321f05efe6423ee0
ephieo <60614102+ephieo@users.noreply.github.com>
2020-04-24 23:16:49 +0100

    adding nextButton event listener so it clicks

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // getting the class name so we can work with the children of this list.
 const carousel = document.querySelector(".carousel");
+// getting the right btn class 
+const nextButton = document.querySelector(".right-btn");
 //turning the children of carousel into an array.
 const slides = [...carousel.children];
 // then using a for loop to loop through all the slides from index/i[0] onwards to the end of it's length. 
@@ -13,3 +15,7 @@ slides[i].style.left=slideWidth * i +"px";
 }
 positionSlides(slides);
 
+//adding an event listener to the nextButton.
+nextButton.addEventListener("click",function(){
+  const currentSlide = carousel.querySelector(".active")
+  }
